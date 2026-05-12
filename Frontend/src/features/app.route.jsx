29a05d1406819615d92f4959995a9ca550a392ce
abiditgogoi01 <abidit.gogoi01@gmail.com>
2026-05-12 14:45:47 +0530

    Frontend updates involving complain sheet fro patient using email but password not yet added for node mailer feature as 2FA not working. Will need to update it

--- Frontend/src/features/app.route.jsx
+++ Frontend/src/features/app.route.jsx
@@ -1,65 +1,88 @@
-import { createBrowserRouter, Navigate } from "react-router-dom"
+import { createBrowserRouter } from "react-router-dom"
+
 import Login from "./auth/pages/Login"
 import Register from "./auth/pages/Register"
+
 import PatientDashboard from "./patient/pages/PatientDashboard"
+
 import DoctorDashboard from "./doctor/pages/DoctorDashboard"
 import DoctorPatientDetails from "./doctor/pages/DoctorPatientDetails"
+
 import AdminDashboard from "../features/admin/pages/AdminDashboard"
+
 import Homepage from "./homepage/homepage"
 import Terms from "./homepage/Terms"
 import Privacy from "./homepage/Privacy"
+import Support from "./homepage/support"
+
 import VideoRoom from "./video/VideoRoom"
+
 import ChatConsultation from "./patient/pages/ChatConsultation"
 import DoctorChatConsultation from "./doctor/pages/DoctorChatConsultation"
-
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Homepage />
     },
+
     {
         path: "/login",
         element: <Login />
     },
+
     {
         path: "/register",
         element: <Register />
     },
+
     {
         path: "/patient-dashboard",
         element: <PatientDashboard />
     },
+
     {
         path: "/patient/chat/:appointmentId",
         element: <ChatConsultation />
     },
+
     {
         path: "/doctor-dashboard",
         element: <DoctorDashboard />
     },
+
     {
         path: "/doctor-dashboard/patient/:patientId",
         element: <DoctorPatientDetails />
     },
+
     {
         path: "/doctor/chat/:appointmentId",
         element: <DoctorChatConsultation />
     },
+
     {
         path: "/admin-dashboard",
         element: <AdminDashboard />
     },
+
     {
         path: "/video-room/:roomId",
         element: <VideoRoom />
     },
+
     {
         path: "/terms",
         element: <Terms />
     },
+
     {
         path: "/privacy",
         element: <Privacy />
+    },
+
+    {
+        path: "/support",
+        element: <Support />
     }
 ])
